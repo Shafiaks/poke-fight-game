6dd0b4b0ad27cd9c02429a65b8b7beadf6d904ad
Shafiaks <113788543+Shafiaks@users.noreply.github.com>
2022-11-29 19:16:24 +0100

    added show more template and style.css

--- poke-fight/src/components/PokemonList.js
+++ poke-fight/src/components/PokemonList.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
 import DataJson from '../DataJson.json'
 import '../styles.css'
 
 function PokemonList() {
-   
-//   {
-//     console.log(DataJson[0].name.english,DataJson.length);
-//   }
-  return (
-    <div>
-      <h3>Pokemon List</h3>
-          { DataJson.map( element =>(
-            <div key={element.id}>
-                <p><span className='poke-Names'>{element.name.english}</span><a href="#">show details</a></p>
-            </div>
-          ))
-        }
+    const [readMore, setReadMore] = useState(false);
+    const extraContent = <div>
+        <p className="extra-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur nequeab
+            porro quasi culpa nulla rerum quis minus voluptatibus sed hic ad quo sint, libero
+            commodi officia aliquam! Maxime.
+        </p>
     </div>
-  )
+    const linkName = readMore ? 'Read Less << ' : 'Read More >> '
+    //   {
+    //     console.log(DataJson[0].name.english,DataJson.length);
+    //   }
+    return (
+        <div>
+            <h2>Pokemon List</h2>
+            {DataJson.map(element => (
+                <div className='name-list' key={element.id}>
+                    <p>
+                        <span className='poke-Name'>{element.name.english}</span>
+                        <a className="read-more-link" onClick={() => { setReadMore(!readMore) }}><span>{linkName}</span>
+                        </a>
+                        {readMore && extraContent}
+                    </p>
+                </div>
+            ))
+            }
+        </div>
+    )
 }
 
 export default PokemonList
