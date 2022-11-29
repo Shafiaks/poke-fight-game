8459caa2f17004dcdb3e0d3313c2c5d3a4acd738
Shafiaks <113788543+Shafiaks@users.noreply.github.com>
2022-11-29 17:14:02 +0100

    added json file, added basic routes

--- poke-fight/src/App.js
+++ poke-fight/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { NavLink, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className='wrapper'>
+        <Routes>
+          <Route path='/' element={<PokemonList />} />
+          <Route path='/pokemon/:id /*' element={<SinglePokemon />}>
+            <Route path=':info' element={<SuperDetailedView />} />
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ poke-fight/src/components/SinglePokemon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SinglePokemon() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SinglePokemon
